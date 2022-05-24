8401f3c1a60b69e835d4b1264ee6635cb176b34d
Matthew Arofin <Matt.arofin@gmail.com>
2022-05-24 16:51:05 -0400

    Framing component builds

---
+++ src/Components/Form.js
@@ -0,0 +1,3 @@
+// Form Component
+
+// Team List in question. Should display basic information of every team member (eg. name, role, etc)
---
+++ src/Components/Member.js
@@ -0,0 +1,3 @@
+// Individual Member info
+
+// Build out a contact card displaying a member's full details (eg. contact details, office hours, etc)
